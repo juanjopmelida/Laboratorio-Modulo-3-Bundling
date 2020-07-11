f557000a7706c29e8b3289373ff59df73739b0d5
Juan José Pérez <juanjoseperez@MacBook-Pro-de-Juan.local>
2020-07-12 00:45:24 +0200

    refactoring

--- webpack.common.js
+++ webpack.common.js
@@ -1,75 +1,74 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
 
 const basePath = __dirname;
 
 module.exports = {
-    context: path.join(basePath, 'src'),
-    resolve: {
-        extensions: [".js", ".ts", ".tsx"]
-    },
-    entry: {
-        app: "./index.tsx",
-        appStyles: ['./mystyles.scss', './averageComponentStyles.scss', './totalScoreComponentStyles.scss']
-    },
-    output: {
-        filename: "[name].[chunkhash].js",
-
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                exclude: /node_modules/,
-                loader: "babel-loader"
+  context: path.join(basePath, "src"),
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"],
+  },
+  entry: {
+    app: "./index.tsx",
+    appStyles: ["./mystyles.scss", "./jokeComponentStyles.scss"],
+  },
+  output: {
+    filename: "[name].[chunkhash].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                localIdentName: "[name]__[local]__[hash:base64:5]",
+              },
+              localsConvention: "camelCase",
             },
-            {
-                test: /\.scss$/,
-                exclude: /node_modules/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader:"css-loader",
-                        options: {
-                            modules: {
-                                localIdentName: "[name]__[local]__[hash:base64:5]"
-                            },
-                            localsConvention: "camelCase"
-                        }
-                    },
-                    {
-                        loader: "sass-loader",
-                        options: {
-                            implementation: require("sass")
-                        },
-                    }
-                ]
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              implementation: require("sass"),
             },
-            {
-                test: /\.css$/,
-                exclude: /node_modules/,
-                use: [ MiniCssExtractPlugin.loader, "css-loader" ] 
-            },
-            {
-                test: /\.(png|jpg)$/,
-                exclude: /node_modules/,
-                loader: "url-loader?limit=5000"
-            }, 
-            {
-                test: /\.html$/,
-                loader: "html-loader"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'index.html',
-        }),
-        new MiniCssExtractPlugin({
-            filename: "[name].css",
-            chunkFilename: "[id].css"
-        })
-    ]
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        exclude: /node_modules/,
+        loader: "url-loader?limit=5000",
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css",
+    }),
+  ],
 };
