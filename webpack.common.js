5785a4fda70d52da35e8b34a76d764460d41609e
Juan José Pérez <juanjoseperez@MacBook-Pro-de-Juan.local>
2020-07-16 23:34:48 +0200

    for delivery

--- webpack.common.js
+++ webpack.common.js
@@ -13,7 +13,7 @@ module.exports = {
   stats: "errors-only",
   entry: {
     app: "./index.tsx",
-    appStyles: ["./mystyles.scss", "./jokeComponentStyles.scss"],
+    appStyles: ["./myStyles.scss", "./jokeComponentStyles.scss"],
     vendorStyles: ["../node_modules/bootstrap/dist/css/bootstrap.css"],
   },
   output: {
